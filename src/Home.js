fbe8f43f07e6ec340a8389a3da6bc045f52a040e
unknown <mohamadhazems@gmail.com>
2021-06-09 22:37:43 -0300

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './Home';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
